cede078b41a3cf0e6c9c2511908c5771f91c1689
hngiap <hngiap94@gmail.com>
2023-06-21 00:59:30 +0700

    commit

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = '33fiq8q7if'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-ahxz3eqmwof8wm76.us.auth0.com',
+  clientId: '1FuZ5agze6tPwAHMVLEXR1nl6Zr0tz6q',
+  callbackUrl: 'http://localhost:3000/callback'
+}
